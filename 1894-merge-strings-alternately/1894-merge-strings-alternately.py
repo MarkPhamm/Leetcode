77b62dbd9ba83ca4ad4137889db579a6cd7b5991
Minh (Mark) Pham <99457952+MarkPhamm@users.noreply.github.com>
2024-10-29 12:09:39 -0500

    Time: 25 ms (97.32%), Space: 16.5 MB (47.29%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.py
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.py
@@ -1,13 +1,26 @@
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
-        result = ""
-        common_length = len(word1) if len(word1) < len(word2) else len(word2)
+        # result = ""
+        # common_length = len(word1) if len(word1) < len(word2) else len(word2)
+        # for i in range(common_length):
+        #     result += word1[i]
+        #     result += word2[i]
+        # result += word1[common_length:]
+        # result += word2[common_length:]
+        # return result
+
+        results = ""
+        common_length = min(len(word1), len(word2))
         for i in range(common_length):
-            result += word1[i]
-            result += word2[i]
-        result += word1[common_length:]
-        result += word2[common_length:]
-        return result
+            results += word1[i]
+            results += word2[i]
+        if len(word1) > common_length:
+            results += word1[common_length:]
+        if len(word2) > common_length:
+            results += word2[common_length:]
+        return results
+
+
 
     
         
