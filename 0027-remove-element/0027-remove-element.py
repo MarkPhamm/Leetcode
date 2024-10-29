cb049f10930c2806f87e7c4efa9b06594198f310
Minh (Mark) Pham <99457952+MarkPhamm@users.noreply.github.com>
2024-10-29 15:42:27 -0500

    Time: 0 ms (100%), Space: 16.5 MB (44.52%) - LeetHub

--- 0027-remove-element/0027-remove-element.py
+++ 0027-remove-element/0027-remove-element.py
@@ -1,16 +1,30 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
+        # n = len(nums)
+        # exchanges = 0
+        # i = 0
+        # while i < n:
+        #     if nums[i] == val:
+        #         nums.pop(i)
+        #         nums.append("_")
+        #         exchanges += 1
+        #         i-=1
+        #     i+=1
+        # print(nums)
+        # return n-exchanges
         n = len(nums)
-        exchanges = 0
         i = 0
+
         while i < n:
             if nums[i] == val:
-                nums.pop(i)
-                nums.append("_")
-                exchanges += 1
-                i-=1
-            i+=1
-        print(nums)
-        return n-exchanges
+                nums[i] = nums[n-1]
+                n-=1
+            else:
+                i+=1
+        return n
+
+
+
+
         
         
