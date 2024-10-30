f8a6e07151ba89436bb26fa4cdfe7bbe0ce3a9b9
Minh (Mark) Pham <99457952+MarkPhamm@users.noreply.github.com>
2024-10-30 15:10:48 -0500

    Time: 11 ms (9.31%), Space: 16.6 MB (31.7%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,13 +1,13 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         while True:
-            if "[]" in s:
-                s = s.replace("[]","",1)
+            if "()" in s:
+                s = s.replace("()","", 1)
             elif "{}" in s:
-                s = s.replace("{}","",1)
-            elif "()" in s:
-                s = s.replace("()","",1)
+                s = s.replace("{}","", 1)
+            elif "[]" in s:
+                s = s.replace("[]","", 1)
             else:
                 break
-        return s == ""
-        
+        return len(s) == 0
+            
