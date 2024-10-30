0747553ce20688767a2b658bfce494545eea46fd
Minh (Mark) Pham <99457952+MarkPhamm@users.noreply.github.com>
2024-10-30 17:22:48 -0500

    Time: 0 ms (100%), Space: 16.6 MB (45.41%) - LeetHub

--- 0027-remove-element/0027-remove-element.py
+++ 0027-remove-element/0027-remove-element.py
@@ -1,19 +1,18 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        n = len(nums)
         i = 0
-        occurence = 0
-        while i < n:
+        j = len(nums)-1 
+        while i <= j:
             if nums[i] == val:
-                nums.pop(i)
-                nums.append("_")
-                i-=1
-                occurence +=1
-            i+=1
-        print(occurence)
-        return n-occurence
+                nums[i] = nums[j]
+                j-=1
+            else:
+                i+=1
+        return j+1
+
 
 # Time complexity(O(n^2))
 # Space complexity((O(1)))
         
 # [0,1,2,2,3,0,4,2] -> [0,1,2,3,0,4,2,"_"] -> [0,1,3,0,4,2,"_","_"] -> [0,1,3,0,4,"_","_","_"]
+
