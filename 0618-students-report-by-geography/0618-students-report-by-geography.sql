1e97b679976c67cc85d1bb5a3eb06988f57bd429
Minh (Mark) Pham <99457952+MarkPhamm@users.noreply.github.com>
2024-10-31 19:58:49 -0500

    Time: 259 ms (85.25%), Space: 0B (100%) - LeetHub

--- 0618-students-report-by-geography/0618-students-report-by-geography.sql
+++ 0618-students-report-by-geography/0618-students-report-by-geography.sql
@@ -5,33 +5,19 @@
 -- CASE WHEN Continent = 'Asia' THEN name else null END AS Asia,
 -- CASE WHEN Continent = 'Europe' THEN name else null END AS Europe
 -- FROM Student
-with america as
+ 
+WITH cte as
 (
 SELECT 
-    row_number() over(ORDER BY name) as id,
-    name as America
+    *, 
+    row_number() over(partition by continent order by name) id
 FROM Student
-WHERE continent = 'America'
-),
-
-asia as
-(
-SELECT 
-    row_number() over(ORDER BY name) as id,
-    name as Asia
-FROM Student
-WHERE continent = 'Asia'
-),
-
-europe as
-(
-SELECT 
-    row_number() over(ORDER BY name) as id,
-    name as Europe
-FROM Student
-WHERE continent = 'Europe'
 )
 
-SELECT America, Asia, Europe FROM america
-LEFT JOIN asia using(id)
-LEFT JOIN europe using(id)
+SELECT 
+MAX(CASE WHEN Continent = 'America' THEN name else null END) AS America,
+MAX(CASE WHEN Continent = 'Asia' THEN name else null END) AS Asia,
+MAX(CASE WHEN Continent = 'Europe' THEN name else null END) AS Europe
+FROM cte 
+GROUP BY id
+ORDER BY id
