ffb534e8550f72ac361300f01ad7244d232416ed
Minh (Mark) Pham <99457952+MarkPhamm@users.noreply.github.com>
2024-11-01 09:51:45 -0500

    Time: 743 ms (90.21%), Space: 0B (100%) - LeetHub

--- 1162-project-employees-ii/1162-project-employees-ii.sql
+++ 1162-project-employees-ii/1162-project-employees-ii.sql
@@ -1,8 +1,21 @@
 # Write your MySQL query statement below
-WITH cte AS
-(
-SELECT project_id, RANK() OVER(ORDER BY COUNT(distinct employee_id) DESC) ranking FROM Project
-GROUP BY 1
+-- WITH cte AS
+-- (
+-- SELECT project_id, RANK() OVER(ORDER BY COUNT(distinct employee_id) DESC) ranking FROM Project
+-- GROUP BY 1
+-- )
+-- SELECT project_id FROM cte 
+-- WHERE ranking = 1
+
+# O(n Log(n))
+
+WITH project_summary AS (
+    SELECT project_id, COUNT(employee_id) AS employee_cnt
+    FROM Project
+    GROUP BY 1
+    ORDER BY 2 DESC
 )
-SELECT project_id FROM cte 
-WHERE ranking = 1
+
+SELECT project_id
+FROM project_summary
+WHERE employee_cnt = (SELECT employee_cnt FROM project_summary LIMIT 1)
