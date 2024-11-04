c332fe3cc95c583df130feeccae7fa129420885f
Minh (Mark) Pham <99457952+MarkPhamm@users.noreply.github.com>
2024-11-03 22:14:44 -0600

    Time: 3 ms (99.11%), Space: 16.7 MB (85.23%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,8 +1,9 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        for i in s:
-            if i in t:
-                s = s.replace(i, "", 1)
-                t = t.replace(i, "", 1)
-        return len(s) == 0 and len(t) == 0
-        
+        if len(s) != len(t):
+            return False
+        nc = set(s)
+        for i in nc:
+            if s.count(i) != t.count(i):
+                return False
+        return True
