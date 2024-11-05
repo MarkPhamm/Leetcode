408c3c8dfec16902ac8afd1d880fb1080c96bf35
Minh (Mark) Pham <99457952+MarkPhamm@users.noreply.github.com>
2024-11-05 09:23:07 -0600

    Time: 639 ms (58.33%), Space: 0B (100%) - LeetHub

---
+++ 1425-weather-type-in-each-country/1425-weather-type-in-each-country.sql
@@ -0,0 +1,14 @@
+# Write your MySQL query statement below
+
+SELECT
+    country_name,
+    CASE 
+        WHEN AVG(weather_state) <= 15 THEN "Cold"
+        WHEN AVG(weather_state) >= 25 THEN "Hot"
+        ELSE "Warm"
+    END AS weather_type
+FROM Weather
+JOIN Countries
+USING(country_id)
+WHERE DATE_FORMAT(day, "%Y-%m") = "2019-11"
+GROUP BY 1
