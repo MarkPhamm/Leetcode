3f6b13a87f738df721fbc0b3fe0fafcb0eeda26f
Minh (Mark) Pham <99457952+MarkPhamm@users.noreply.github.com>
2024-11-05 17:11:05 -0600

    Time: 484 ms (32.33%), Space: 0B (100%) - LeetHub

--- 1420-find-the-start-and-end-number-of-continuous-ranges/1420-find-the-start-and-end-number-of-continuous-ranges.sql
+++ 1420-find-the-start-and-end-number-of-continuous-ranges/1420-find-the-start-and-end-number-of-continuous-ranges.sql
@@ -1,11 +1,30 @@
 # Write your MySQL query statement below
 
-with CTE AS
+-- with CTE AS
+-- (
+--     SELECT 
+--         log_id,
+--         log_id - ROW_NUMBER() OVER(ORDER BY log_id) as group_id
+--     FROM Logs
+-- )
+
+-- SELECT 
+--     MIN(LOG_ID) AS start_id , 
+--     MAX(log_id) AS end_id
+-- FROM CTE
+-- GROUP BY group_id
+
+
+# Gaps and Island Solution
+WITH CTE AS
 (
-    SELECT 
-        log_id,
-        log_id - ROW_NUMBER() OVER(ORDER BY log_id) as group_id
-    FROM Logs
+SELECT log_id, SUM(start_signal) OVER(ORDER BY log_id) AS group_id FROM 
+(
+SELECT
+    *,
+    IF(LAG(log_id) OVER(ORDER BY log_Id) IS NULL OR LAG(log_id) OVER(ORDER BY log_Id) != log_id-1,1,0) AS start_signal
+FROM Logs
+) a
 )
 
 SELECT 
