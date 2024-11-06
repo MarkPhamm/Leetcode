ffaab7c8dd6585c94c08ec110df1bf7633dacfae
Minh (Mark) Pham <99457952+MarkPhamm@users.noreply.github.com>
2024-11-06 09:51:21 -0600

    Time: 1123 ms (24.51%), Space: 0B (100%) - LeetHub

--- 1481-students-with-invalid-departments/1481-students-with-invalid-departments.sql
+++ 1481-students-with-invalid-departments/1481-students-with-invalid-departments.sql
@@ -1,4 +1,7 @@
-# Write your MySQL query statement below
+-- # Write your MySQL query statement below
 
-SELECT distinct id, name FROM Students
-WHERE department_id NOT IN (SELECT distinct id FROM Departments)
+-- SELECT distinct id, name FROM Students
+-- WHERE department_id NOT IN (SELECT distinct id FROM Departments)
+SELECT s.id, s.name FROM students s
+LEFT JOIN departments d ON d.id = department_id
+WHERE d.id IS NULL;
