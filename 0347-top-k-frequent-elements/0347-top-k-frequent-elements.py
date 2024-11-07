e14891004087105b24a9d88ad5e5e0f08c74d209
Minh (Mark) Pham <99457952+MarkPhamm@users.noreply.github.com>
2024-11-07 13:08:02 -0600

    Time: 15 ms (11.77%), Space: 23.3 MB (6.37%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,20 +1,29 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # Bucket sort
         count = {}
-        frequency = [[] for i in range(len(nums)+1)]
-        
-        # Initiate the count
-        for num in nums:
-            count[num] = count.get(num,0) + 1
-        
-        # sort the count into buckets, each buckets as the number of occurence
-        for n, cnt in count.items():
-            frequency[cnt].append(n)
+        # Create a frequency array
+        freq = [[] for i in range(len(nums) + 1)]
 
-        ans = []
-        for i in range(len(frequency)-1, 0,-1):
-            for n in frequency[i]:
-                ans.append(n)
-                if len(ans) == k:
-                    return ans
+        # Count the frequency of each element
+        for num in nums:
+            count[num] = 1 + count.get(num, 0)
+
+        # Add the elements to the frequency array
+        for num, cnt in count.items():
+            freq[cnt].append(num)
+        
+        print("The frequency array is: ", freq)
     
+        # Get the k most frequent elements
+        res = []
+        # Iterate through the frequency array in reverse order
+        for i in range(len(freq)-1, 0, -1):
+            # Iterate through the elements in the current frequency bucket
+            for n in freq[i]:
+                # Add the element to the result list
+                res.append(n)
+                # If we have collected k elements, return the result
+                if len(res) == k:
+                    return res
+
