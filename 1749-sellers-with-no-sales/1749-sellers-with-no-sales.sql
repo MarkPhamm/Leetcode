f968e7082d3c1018a19dc56911de2906a4e840a4
Minh (Mark) Pham <99457952+MarkPhamm@users.noreply.github.com>
2024-11-11 13:19:53 -0600

    Time: 1213 ms (27.53%), Space: 0B (100%) - LeetHub

--- 1749-sellers-with-no-sales/1749-sellers-with-no-sales.sql
+++ 1749-sellers-with-no-sales/1749-sellers-with-no-sales.sql
@@ -1,7 +1,7 @@
 # Write your MySQL query statement below
 select s.seller_name from Seller s
 where not exists (
-    select 1 from Orders o
-    where o.seller_id = s.seller_id and year(sale_date) = 2020
+select order_id from Orders o
+where o.seller_id = s.seller_id and year(sale_date) = 2020
 )
 order by seller_name
