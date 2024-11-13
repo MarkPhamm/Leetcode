622727742671c68d5cb267a02433365c62a83b86
Minh (Mark) Pham <99457952+MarkPhamm@users.noreply.github.com>
2024-11-13 11:11:53 -0600

    Time: 3 ms (92.11%), Space: 16.6 MB (24.73%) - LeetHub

--- 1542-consecutive-characters/1542-consecutive-characters.py
+++ 1542-consecutive-characters/1542-consecutive-characters.py
@@ -1,18 +1,31 @@
 class Solution:
     def maxPower(self, s: str) -> int:
-        i = 0
-        j = 0
-        longest = 0
+        # Two Pointer
+        # i = 0
+        # j = 0
+        # longest = 0
 
-        s = s+"0"
+        # s = s+"0"
 
-        while i < len(s):
-            if s[i] != s[j]:
-                longest = max(longest, i-j)
-                j = i
-            i+=1
+        # while i < len(s):
+        #     if s[i] != s[j]:
+        #         longest = max(longest, i-j)
+        #         j = i
+        #     i+=1
         
-        return longest
+        # return longest
+        
+        current_sum = 0
+        max_sum = 0
+        for i in range(1, len(s)):
+            if s[i] == s[i-1]:
+                current_sum +=1
+                max_sum = max(current_sum, max_sum)
+            else:
+                current_sum = 0
+        
+        return max_sum+1
+
 
 
         
