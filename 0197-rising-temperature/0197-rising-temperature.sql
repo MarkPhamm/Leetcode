4f14e534112b929dbf6e579fec41cfed22f12027
Minh (Mark) Pham <99457952+MarkPhamm@users.noreply.github.com>
2024-11-16 14:53:35 -0600

    Time: 391 ms (91.31%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,3 +1,6 @@
 # Write your MySQL query statement below
-select b.id from weather a,weather b 
-where Datediff (b.RecordDate, a.RecordDate) =1  and a.temperature < b.temperature 
+# Write your MySQL query statement below
+select b.id from weather a 
+join weather b 
+on a.recordDate  + INTERVAL 1 DAY = b.recordDate
+AND a.temperature < b.temperature 
