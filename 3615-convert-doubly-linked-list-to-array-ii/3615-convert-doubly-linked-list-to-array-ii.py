8aadda03d941ff385c6d3298c6178ef68f63e5e2
Minh (Mark) Pham <99457952+MarkPhamm@users.noreply.github.com>
2024-11-25 21:32:16 -0600

    Time: 78 ms (54.78%), Space: 18.3 MB (48.59%) - LeetHub

---
+++ 3615-convert-doubly-linked-list-to-array-ii/3615-convert-doubly-linked-list-to-array-ii.py
@@ -0,0 +1,21 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val, prev=None, next=None):
+        self.val = val
+        self.prev = prev
+        self.next = next
+"""
+class Solution:
+    def toArray(self, node: 'Optional[Node]') -> List[int]:
+        cur = node
+        while cur and cur.prev:
+            cur = cur.prev
+        print(cur.val)
+        ans = []
+        while cur:
+            ans.append(cur.val)
+            cur = cur.next
+        
+        return ans
+        
