c89c4bc2b1bb2fb1516e93354fa813f85bc79bb4
Minh (Mark) Pham <99457952+MarkPhamm@users.noreply.github.com>
2024-11-29 10:56:38 -0600

    Time: 357 ms (21.75%), Space: 17.2 MB (13.2%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.py
+++ 1013-fibonacci-number/1013-fibonacci-number.py
@@ -1,10 +1,9 @@
-class Solution:
-    def fib(self, n: int) -> int:
-        if n < 2:
-            return n
-        memo = [0,0,1]
-        for i in range (2,n+1):
-            memo[0] = memo[1]
-            memo[1] = memo[2]
-            memo[2] = memo[1] + memo[0]
-        return memo[2]
+@cache
+class Solution():
+    def fib(self, n):
+        if n == 0:
+            return 0
+        elif n == 1:
+            return 1
+        else:
+            return self.fib(n-1)+self.fib(n-2)
