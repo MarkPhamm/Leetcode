047dfd33811035a191664fd47c6f394a8b6d17c4
Minh (Mark) Pham <99457952+MarkPhamm@users.noreply.github.com>
2024-12-01 14:01:08 -0600

    Time: 119 ms (99.71%), Space: 19.4 MB (6.02%) - LeetHub

--- 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.py
+++ 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.py
@@ -1,14 +1,15 @@
 class Solution:
     def makeFancyString(self, s: str) -> str:
-        i = 0
-        threshold = len(s)-3
-        while i <= threshold:
-            if s[i] == s[i+1] and s[i] == s[i+2]:
-                s = s[:i]+s[i+1:]
-                threshold -=1
+        has_two = False
+        prev = None
+        result = []
+        for x in s:
+            if prev == x:
+                if not has_two:
+                    result.append(x)
+                    has_two = True
             else:
-                i+=1
-        return s
-
-
-        
+                result.append(x)
+                prev = x
+                has_two = False
+        return ''.join(result)
