39ef4a0886e4811233eba5c37ca2f39c582ebbc7
Minh (Mark) Pham <99457952+MarkPhamm@users.noreply.github.com>
2024-12-07 20:34:36 -0600

    Time: 55 ms (29.59%), Space: 23 MB (5.92%) - LeetHub

--- 0969-number-of-recent-calls/0969-number-of-recent-calls.py
+++ 0969-number-of-recent-calls/0969-number-of-recent-calls.py
@@ -6,8 +6,7 @@ class RecentCounter:
 
     def ping(self, t: int) -> int:
         self.requests.append(t)
-        min_time = t-3000
-        while self.requests and self.requests[0] < min_time:
+        while self.requests and self.requests[0]< t-3000:
                 self.requests.pop(0)
         
         return len(self.requests)
