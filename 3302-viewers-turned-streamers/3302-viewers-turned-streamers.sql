ac16d4b96381b0ce3e6f48582a63d1ecdf11172e
Minh (Mark) Pham <99457952+MarkPhamm@users.noreply.github.com>
2024-12-09 11:32:58 -0600

    Time: 395 ms (26.88%), Space: 0B (100%) - LeetHub

--- 3302-viewers-turned-streamers/3302-viewers-turned-streamers.sql
+++ 3302-viewers-turned-streamers/3302-viewers-turned-streamers.sql
@@ -1,19 +1,21 @@
-# Write your MySQL query statement below
+-- Write your MySQL query statement below
 
-WITH first_as_viewers AS
-(
-SELECT user_id FROM 
-(
-SELECT 
-    *,
-    RANK() OVER(PARTITION BY user_id ORDER BY session_start) rnk
-FROM Sessions
-) a
-WHERE rnk = 1 AND session_type = 'Viewer'
+WITH first_as_viewers AS (
+    SELECT user_id 
+    FROM (
+        SELECT 
+            *,
+            RANK() OVER (PARTITION BY user_id ORDER BY session_start) AS rnk
+        FROM Sessions
+    ) a
+    WHERE rnk = 1 AND session_type = 'Viewer'
 )
 
-SELECT user_id, COUNT(session_id) sessions_count FROM Sessions
-WHERE user_id in (SELECT user_id FROM first_as_viewers)
-AND session_type = 'Streamer'
-GROUP BY 1
-ORDER BY 2 DESC, 1 DESC
+SELECT 
+    user_id, 
+    COUNT(session_id) AS sessions_count 
+FROM Sessions
+WHERE user_id IN (SELECT user_id FROM first_as_viewers)
+  AND session_type = 'Streamer'
+GROUP BY user_id
+ORDER BY sessions_count DESC, user_id DESC;
