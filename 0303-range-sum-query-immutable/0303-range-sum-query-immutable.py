e4ce328dbdad028bedfed6226f153f5b4c082860
Minh (Mark) Pham <99457952+MarkPhamm@users.noreply.github.com>
2025-02-02 13:55:03 -0600

    Time: 3 ms (74.26%), Space: 21.9 MB (6.74%) - LeetHub

--- 0303-range-sum-query-immutable/0303-range-sum-query-immutable.py
+++ 0303-range-sum-query-immutable/0303-range-sum-query-immutable.py
@@ -1,8 +1,30 @@
+# Prefix Sum Question
 class NumArray:
-    def __init__(self, nums: List[int]):
-        self.prefix_sum = [0] * (len(nums) + 1)  # Use an extra space to handle prefix_sum[-1] = 0
-        for i in range(len(nums)):
-            self.prefix_sum[i + 1] = self.prefix_sum[i] + nums[i]
 
+    def __init__(self, nums: List[int]):
+        self.nums = nums
+        # create attribute prefix sum
+        # SC: O(n)
+        # TC: O(n)
+        self.prefix_sum = [0]
+        for num in self.nums:
+            self.prefix_sum.append(num + self.prefix_sum[-1])
+        
     def sumRange(self, left: int, right: int) -> int:
-        return self.prefix_sum[right + 1] - self.prefix_sum[left]
+        # TC: O(right-left)
+        # SC: O(1)
+        # current_sum = 0
+        # for i in range(left, right+1):
+        #     current_sum+=self.nums[i]
+        # return current_sum
+
+        # TC: O(1)
+        # SC: O(1)
+        # sum range = prefix(r+1) - prefix(l) become O(1)
+        return self.prefix_sum[right+1] - self.prefix_sum[left]
+        
+
+
+# Your NumArray object will be instantiated and called as such:
+# obj = NumArray(nums)
+# param_1 = obj.sumRange(left,right)
