635f3430b81bce4ecf30df73305f7815db4ea09e
Minh (Mark) Pham <99457952+MarkPhamm@users.noreply.github.com>
2025-02-02 14:00:58 -0600

    Time: 3 ms (12.77%), Space: 17.6 MB (81.56%) - LeetHub

--- 1848-sum-of-unique-elements/1848-sum-of-unique-elements.py
+++ 1848-sum-of-unique-elements/1848-sum-of-unique-elements.py
@@ -1,17 +1,18 @@
-import counter
 class Solution:
     def sumOfUnique(self, nums: List[int]) -> int:
         # TC: O(N)
         # SC: O(1)
 
-        hashmap = {}
-        for num in nums:
-            hashmap[num] = hashmap.get(num, 0)+1
+        # hashmap = {}
+        # for num in nums:
+        #     hashmap[num] = hashmap.get(num, 0)+1
         
-        ans = 0
-        for key, value in hashmap.items():
-            if value == 1:
-                ans += key
+        # ans = 0
+        # for key, value in hashmap.items():
+        #     if value == 1:
+        #         ans += key
         
-        return ans
+        # return ans
+
+        return sum(num for num in nums if nums.count(num) == 1)
         
