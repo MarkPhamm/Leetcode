4a96f575a6ae58c78562acafe115500594930f7d
Minh (Mark) Pham <99457952+MarkPhamm@users.noreply.github.com>
2025-02-03 10:46:06 -0600

    Time: 2 ms (59.22%), Space: 17.5 MB (77.91%) - LeetHub

--- 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/3372-longest-strictly-increasing-or-strictly-decreasing-subarray.py
+++ 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/3372-longest-strictly-increasing-or-strictly-decreasing-subarray.py
@@ -5,15 +5,24 @@ class Solution:
         current_asc = current_desc = 1
         
         for i in range(n-1):
+            # if the next number is larger than the current number
             if nums[i+1] > nums[i]:
+                # increase current_ascending
                 current_asc += 1
+                # update max_asc
                 max_asc = max(max_asc, current_asc)
+                # reset current_descending
                 current_desc = 1
+            # if the next number is smaller than the current number
             elif nums[i+1] < nums[i]:
+                # increase current_descending
                 current_desc += 1
+                # update max_desc
                 max_desc = max(max_desc, current_desc)
+                # reset current_ascending
                 current_asc = 1
             else:
+                # reset both current_asc and current_desc
                 current_asc = current_desc = 1
         
         return max(max_asc, max_desc)
