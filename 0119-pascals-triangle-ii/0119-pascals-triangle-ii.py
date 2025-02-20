5b88ab36fc34b00944ccd704d025cf5a4d6bcff5
Minh (Mark) Pham <99457952+MarkPhamm@users.noreply.github.com>
2025-02-20 13:53:35 -0600

    Time: 0 ms (100%), Space: 18 MB (11.56%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
@@ -1,19 +1,22 @@
 class Solution:
     def getRow(self, rowIndex: int) -> List[int]:
-        if rowIndex == 0:
-            return [1]
-        if rowIndex == 1:
-            return [1,1]
-        
-        base = [[1], [1,1]]
-        while True:
-            mid = []
-            for i in range (len(base[-1])-1):
-                mid.append(base[-1][i] + base[-1][i+1])
-            
-            base.append([1] + mid +[1])
-            if len(base[-1]) == rowIndex+1:
-                return base[-1]
-            
 
+        # Helper recursive function
+        # i: row, j: column
+        @lru_cache(None)
+        def F(i,j):
+            if j == 0 or i == j:
+                return 1
+            return F(i-1,j-1) + F(i-1,j)
+        
+        ans = []
+        for j in range(rowIndex+1):
+            ans.append(F(rowIndex,j))
+        
+        return ans
+        
+
+        
+
+            
         
