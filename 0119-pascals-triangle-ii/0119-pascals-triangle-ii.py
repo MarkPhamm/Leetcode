8026ea940d3d26efccbb116e4110ed06e594e0dc
Minh (Mark) Pham <99457952+MarkPhamm@users.noreply.github.com>
2025-02-20 13:56:52 -0600

    Time: 0 ms (100%), Space: 18.3 MB (11.56%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
@@ -1,5 +1,9 @@
 class Solution:
     def getRow(self, rowIndex: int) -> List[int]:
+        # Step 1: Define F: F(i,j) = row i column j
+        # Step 2: Base case: F(i, 0) = F(i, i) = 1 with every i (on the edges and from the top)
+        # Step 3: Recursive relation: F(i, j): = F(i -1, j - 1) + F(i - 1, j) 
+        # Step 4: Implementation
 
         # Helper recursive function
         # i: row, j: column
@@ -9,10 +13,10 @@ class Solution:
                 return 1
             return F(i-1,j-1) + F(i-1,j)
         
+        # Creating the index
         ans = []
         for j in range(rowIndex+1):
             ans.append(F(rowIndex,j))
-        
         return ans
         
 
