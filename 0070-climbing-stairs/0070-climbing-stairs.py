4e6b4b4f4a369c3bedfedae27eaa211845f8dfac
Minh (Mark) Pham <99457952+MarkPhamm@users.noreply.github.com>
2025-02-21 13:30:14 -0600

    Time: 0 ms (100%), Space: 17.9 MB (8.28%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,8 +1,10 @@
 class Solution:
+    # Base case: n <= 2: F(n) = n ways to climb the stairs
+    # if n > 2: there are F(n) = F(n-1) + F(n-2)
+    @lru_cache(None)
     def climbStairs(self, n: int) -> int:
-        dp = [0] * (n + 1)
-        dp[0] = 1
-        dp[1] = 1
-        for i in range (2,n+1):
-            dp[i] = dp[i-1] + dp[i-2]
-        return dp[-1]
+        if n <= 2:
+            return n
+        else:
+            return self.climbStairs(n-1) + self.climbStairs(n-2)
+        
