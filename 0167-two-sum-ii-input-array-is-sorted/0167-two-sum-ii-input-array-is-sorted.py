8f83bd4f8ec3e125e48b7d35429291c4a448b36a
Minh (Mark) Pham <99457952+MarkPhamm@users.noreply.github.com>
2025-03-08 15:24:14 -0600

    Time: 19 ms (6.38%), Space: 18.9 MB (9.84%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -1,10 +1,25 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        i = 0
-        j = len(numbers)-1
-        while numbers[i] + numbers[j] != target:
-            if numbers[i] + numbers[j] > target:
-                j -=1
-            else:
-                i+=1
-        return [i+1,j+1]
+        def two_pointer(self, numbers: List[int], target: int) -> List[int]:
+            # TC: O(n)
+            # SC: O(1)
+            i = 0
+            j = len(numbers)-1
+            while numbers[i] + numbers[j] != target:
+                if numbers[i] + numbers[j] > target:
+                    j-=1
+                else:
+                    i+=1
+            return [i+1,j+1]
+
+        def hashmap(self, numbers: List[int], target: int) -> List[int]:
+            hashmap = {}
+
+            for index, value in enumerate(numbers):
+                print(index, value)
+                if target - value in hashmap:
+                    return [hashmap[target - value]+1, index+1]
+                if value not in hashmap:
+                    hashmap[value] = index
+        
+        return hashmap(self, numbers, target)
