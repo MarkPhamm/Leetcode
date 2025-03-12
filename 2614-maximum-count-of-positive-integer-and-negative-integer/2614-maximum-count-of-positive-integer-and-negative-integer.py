a12a2ecb12c0af4c4040a51ba165e02b1a3e07ce
Minh (Mark) Pham <99457952+MarkPhamm@users.noreply.github.com>
2025-03-11 20:21:07 -0500

    Time: 4 ms (12.81%), Space: 18 MB (41.77%) - LeetHub

--- 2614-maximum-count-of-positive-integer-and-negative-integer/2614-maximum-count-of-positive-integer-and-negative-integer.py
+++ 2614-maximum-count-of-positive-integer-and-negative-integer/2614-maximum-count-of-positive-integer-and-negative-integer.py
@@ -1,42 +1,40 @@
 class Solution:
     def maximumCount(self, nums: List[int]) -> int:
-        def brute_force(self, nums: List[int]) -> int:
-            # TC: O(N)
-            # SC: O(1)
-            count_positive = 0
-            count_negative = 0
-            for num in nums:
-                if num > 0:
-                    count_positive +=1
-                elif num < 0:
-                    count_negative +=1
-            return max(count_positive, count_negative)
-        
-        def binary_search(self, nums: List[int]) -> int:
-            # hint: use bisect left and bisec right
-            # lower bound(0)-1 to find last neg 
-            l, r = 0, len(nums)-1
-            last_neg = len(nums)
-            
-            while l <= r:
+        def bisect_left(nums, target):
+            """
+            find the index when insert, all the numbers to it right is LARGER OR EQUAL to target
+            Eg: bisect_right([-3,-2,-1,0,0,1,2],0) return 3
+            """
+            l,r = 0, len(nums)
+            ans = len(nums)
+            while l<r:
                 mid = (l+r)//2
-                if nums[mid] >= 0:
-                    last_neg = mid
-                    r = mid -1
+                if nums[mid] >= target:
+                    ans = mid
+                    r = mid
                 else:
                     l = mid + 1
-            
-            # upper bound(0) to find first pos
-            l, r = 0, len(nums)-1
-            first_pos = len(nums)
+            return ans
 
-            while l <= r:
+        def bisect_right(nums, target):
+            """
+            find the index when insert, all the numbers to it right is LARGER THAN target
+            Eg: bisect_right([-3,-2,-1,0,0,1,2],0) return 5, 
+            from there, we can calculate the number of positive number by taking len(nums) - ans
+            """
+            l,r = 0, len(nums)
+            ans = len(nums)
+            while l<r:
                 mid = (l+r)//2
-                if nums[mid] > 0:
-                    first_pos = mid
-                    r = mid -1
+                if nums[mid] > target:
+                    ans = mid
+                    r = mid
                 else:
-                    l = mid +1
-            
-            return max(last_neg, len(nums) - first_pos)
-        return binary_search(self, nums)
+                    l = mid + 1
+            return ans
+
+
+        last_neg = bisect_left(nums, 0)
+        first_pos = bisect_right(nums, 0)
+        print(last_neg, first_pos)
+        return max(last_neg, len(nums) - first_pos)
