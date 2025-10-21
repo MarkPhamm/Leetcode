2146ad87ffd08fd85df0a524889eb92092918e0b
Minh (Mark) Pham <99457952+MarkPhamm@users.noreply.github.com>
2025-10-20 22:19:02 -0400

    Time: 0 ms (100%), Space: 18.8 MB (74.48%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -1,10 +1,22 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        j = 1
-        for i in range(1, len(nums)):
-            if nums[i] != nums[i-1]:
-                nums[j] = nums[i]
-                j+=1
+        # Solution 1: to delete method
+        # to_delete = []
+        # for i in range(1, len(nums)):
+        #     if nums[i] == nums[i-1]:
+        #         to_delete.append(i)
         
-        return j
-   
+        # for index, delete in enumerate(to_delete):
+        #     nums.pop(delete-index)
+        
+        # return len(nums)
+
+        # Solution 2: two pointer: fast flow pointer Time O(n) , Space O(1)
+        s = 1 
+        for f in range(1, len(nums)): 
+            if nums[f] != nums[f-1]: # check if fast pointer diff fast at index -1 
+               nums[s] = nums[f]
+               s+=1
+            else:
+                pass # else just increase the fast pointer
+        return s
