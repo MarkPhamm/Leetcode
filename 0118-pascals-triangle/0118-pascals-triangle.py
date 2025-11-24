c231a5ad206fe14bbbf0f303dfa26c94b01bbc79
Minh (Mark) Pham <99457952+MarkPhamm@users.noreply.github.com>
2025-11-24 14:53:11 -0500

    Time: 0 ms (100%), Space: 17.6 MB (86.91%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.py
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -1,39 +1,16 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
-        # Step 1: Define F: F(i,j) = row i column j
-        # Step 2: Base case: F(i, 0) = F(i, i) = 1 with every i (on the edges and from the top)
-        # Step 3: Recursive relation: F(i, j): = F(i -1, j - 1) + F(i - 1, j) 
-        # Step 4: Implementation
+        triangle = []
 
-        # F(3,3) = F(2,2) + F(2,3) = F(1,1) + F(1,2) + F(1,2) + F(1,3)
-        # = F(1,1) + 2F(1,2) + F(1,3)
-        # = F(0,0) + F(0,1) + 2(F(0,1) + F(0,2)) + F(0,3)
-        # ^n Exponential 
-        # TLO: TC: (O(x^n)) without memorization, O(n^2) with memorization
-        # SC: O(N^2) = O(N) Recursion + O(N^2) memo
-
-        # F(n) --> F(n-1) --> F(n-2) --> ...
-        # F(0) --> F(1) --> F(n)
-
-        # cache memorization, every (i,j) we calculate only once -> only need to calculate all i,j pair
-        memo = {}
-
-        # Helper recursive function
-        def F(i: int,j: int):
-            if (i,j) in memo: # check the memo:
-                return memo[i,j]
+        for row_num in range(numRows):
+            row = [0 for _ in range(row_num+1)]
+            row[0], row[-1] = 1,1
             
-            if j == 0 or j == i:
-                return 1
-            return_value = F(i-1, j-1) + F(i-1, j)
-            memo[i,j] = return_value
-            return return_value
+            for j in range(1, len(row)-1):
+                prev_row = row_num - 1
+                row[j] = triangle[prev_row][j - 1] + triangle[prev_row][j]
+            
+            triangle.append(row)
+        return triangle
 
-        ans = []
-        for i in range(numRows):
-            ans.append([])
-            for j in range(i+1):
-                ans[-1].append(F(i,j))
         
-        print(memo)
-        return ans
