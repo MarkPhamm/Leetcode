1230a2a37bdc8252158559b1275e14e8f5319fc1
Minh (Mark) Pham <99457952+MarkPhamm@users.noreply.github.com>
2025-11-25 08:18:38 -0500

    Time: 249 ms (51.98%), Space: 0B (100%) - LeetHub

--- 0613-shortest-distance-in-a-line/0613-shortest-distance-in-a-line.sql
+++ 0613-shortest-distance-in-a-line/0613-shortest-distance-in-a-line.sql
@@ -1,6 +1,9 @@
 # Write your MySQL query statement below
-with cte as (select
-x, lag(x,1) over(order by x) as px
-from Point)
+with cte as (
+    select
+        x, 
+        lag(x,1) over(order by x) as px
+    from Point
+)
 
 select min(abs(x-px)) as shortest from cte
