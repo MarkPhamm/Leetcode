a4b9557884ad2f31ab736da1d2158f33f514c706
Minh (Mark) Pham <99457952+MarkPhamm@users.noreply.github.com>
2026-01-27 18:24:18 -0500

    Time: 23 ms (17.39%), Space: 20.7 MB (13.95%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.py
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.py
@@ -1,21 +1,24 @@
 class Solution:
     def reverseVowels(self, s: str) -> str:
         vowels = ["a","e","i","o","u"]
-        s_list = list(s) 
+        s_list = list(s)
 
-        l = 0
-        r = len(s_list)-1
-        while l <= r:
-            while l <= r and s_list[l].lower() not in vowels:
-                l+=1
-            while r >= l and s_list[r].lower() not in vowels:
+        l = 0 
+        r = len(s)-1
+        if len(s) == 1:
+            return s
+
+        while l < r:
+            if s_list[l].lower() in vowels and s_list[r].lower() not in vowels:
                 r-=1
-            if l<=r and s_list[l].lower() in vowels and s_list[r].lower() in vowels:
-                s_list[l],s_list[r] = s_list[r],s_list[l]
-            l+=1
-            r-=1
-        
+            elif s_list[l].lower() not in vowels and s_list[r].lower() in vowels:
+                l+=1
+            elif s_list[l].lower() in vowels and s_list[r].lower() in vowels:
+                s_list[l], s_list[r] = s_list[r], s_list[l]
+                l+=1
+                r-=1
+            else: 
+                l+=1
+                r-=1
         return "".join(s_list)
-                        
-            
         
