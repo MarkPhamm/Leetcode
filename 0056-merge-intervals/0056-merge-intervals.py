d5aea453fcccd9192e75688aaadaf90cca2ba72b
Minh (Mark) Pham <99457952+MarkPhamm@users.noreply.github.com>
2026-02-07 09:07:56 -0500

    Time: 8 ms (44.86%), Space: 22.7 MB (29.16%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -3,10 +3,9 @@ class Solution:
         intervals.sort()
         ans = [intervals[0]]
 
-        # [[1,3],[2,6],[8,10],[15,18]
         for interval in intervals[1:]:
             last_interval = ans[-1]
-            if interval[0] <= last_interval[1]: # case of overlap
+            if interval[0] <= last_interval[1]:
                 last_interval[1] = max(last_interval[1], interval[1])
             else:
                 ans.append(interval)
