91cd740df2dbf806fb2671857540b908a308f2c8
Minh (Mark) Pham <99457952+MarkPhamm@users.noreply.github.com>
2026-02-08 10:40:15 -0500

    Time: 3 ms (52.08%), Space: 21 MB (12.15%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,12 +1,14 @@
-#Loop through each element x and find if there is another value that equals to (target - x)
+# TLDR: # store the compensate and current index 
+# Eg: nums = [2,7,11,15] seen = {7:0}
+# if found current pair: reuturn 
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         seen = {}
         for k,v in enumerate(nums):
+            # print(seen)
             if v not in seen:
                 seen[target-v] = k # store the compensate and current index
             else: 
                 return [seen[v], k] # if we found the pair
-        print(seen)
         
