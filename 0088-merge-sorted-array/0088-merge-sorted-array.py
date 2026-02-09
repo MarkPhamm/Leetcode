7a71997233be2f599a4b2a03850f75477ec642c4
Minh (Mark) Pham <99457952+MarkPhamm@users.noreply.github.com>
2026-02-09 10:27:21 -0500

    Time: 0 ms (100%), Space: 19.1 MB (58.87%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -3,45 +3,22 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        # TC = O(M+N)
-        # SC = O(1)
 
-        # a: [1,2,3,0,0,0]
-        #               i    
-        # b: [2,5,6]
-        #         j
+        k = m + n - 1
 
-        # []
-        # [1,2,2,3,5,6]
-        
-        # two pointer method like merge sort from the bottom of nums1, nums2
-        # Compare nums1[i], nums2[j], which one is larger then deducted it by 1, and add to nums1
-        # Until nums1 or num2 is empty
-
-        # Two pointer (Reader pointer)
-        i, j = m - 1, n - 1
-
-        # Additional pointer to insert the merges number in (Writer pointer)
-        k = m + n - 1 
-
-        # While there's still element in the list
-        while i >= 0 and j >= 0:
-            # compare
-            if nums1[i] > nums2[j]:
-                nums1[k] = nums1[i]
-                # move i pointer
-                i -= 1
+        while m > 0 and n > 0: 
+            if nums1[m-1] > nums2[n-1]:
+                nums1[k] = nums1[m-1]
+                m -= 1
             else:
-                nums1[k] = nums2[j]
-                # move j pointer
-                j -= 1
-            # move k to write the next element
+                nums1[k] = nums2[n-1]    
+                n -= 1
+            k-=1 
+        
+        while n > 0:
+            nums1[k] = nums2[n-1]
             k -= 1
-
-        # copy the rest of j
-        while j>=0:
-            nums1[k] = nums2[j]
-            j-=1
-            k-=1
-
-
+            n -= 1
+            
+        return nums1
+        
