69249d29265b76dcacc0b0eb1ad7b2ac703c11ab
Minh (Mark) Pham <99457952+MarkPhamm@users.noreply.github.com>
2026-02-12 19:51:48 -0500

    Time: 5 ms (93.58%), Space: 19.4 MB (49.57%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,17 +1,5 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if set(s) != set(t):
-            return False
-
-        lookup = {}
-        for char in s:
-            lookup[char] = lookup.get(char, 0) + 1
-        
-        for char in t:
-            lookup[char] = lookup.get(char, 0) - 1
-            if lookup[char] == 0:
-                del(lookup[char])
-        
-        return len(lookup) == 0
+        return Counter(s)==Counter(t)
 
         
