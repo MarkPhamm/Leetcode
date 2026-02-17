f407f40378a2f67a7b53171d3cfc123371b22853
Minh (Mark) Pham <99457952+MarkPhamm@users.noreply.github.com>
2026-02-17 07:33:11 -0500

    Time: 0 ms (100%), Space: 19.5 MB (5.05%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -7,8 +7,8 @@ class Solution:
         for i in range(len(first_str)):
             check_char = first_str[i]
             for remaining_str in strs[1:]:
-                if i == len(remaining_str) or check_char !=  remaining_str[i]: # check for all the string
-                    return ans
-            ans += check_char # valid then add 
-        return ans 
+                if  i == len(remaining_str) or check_char != remaining_str[i]:
+                    return ans 
+            ans += check_char
         
+        return ans
