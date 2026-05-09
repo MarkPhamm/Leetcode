7bc2363aae155f0c655a8b6bb05e2a0b5ea6b4df
Minh (Mark) Pham <99457952+MarkPhamm@users.noreply.github.com>
2026-05-09 15:38:13 -0400

    Time: 970 ms (20.47%), Space: 23.7 MB (69.4%) - LeetHub

--- 0170-two-sum-iii-data-structure-design/0170-two-sum-iii-data-structure-design.py
+++ 0170-two-sum-iii-data-structure-design/0170-two-sum-iii-data-structure-design.py
@@ -1,20 +1,25 @@
 class TwoSum:
 
     def __init__(self):
-        self.list = []
-        
+        self.arr = []
+
 
     def add(self, number: int) -> None:
-        self.list.append(number)
+        self.arr.append(number)
+        return None
 
     def find(self, value: int) -> bool:
         hashmap = {}
-        for n in self.list:
-            if n in hashmap:
-                return True
-            hashmap[value-n] = n
+        for num in self.arr:
+            if num in hashmap:
+                return True 
+                break 
+            else:
+                hashmap[value - num] = num
         return False
-            
+
+# [1,3,5]
+# [3:1]
 # Your TwoSum object will be instantiated and called as such:
 # obj = TwoSum()
 # obj.add(number)
