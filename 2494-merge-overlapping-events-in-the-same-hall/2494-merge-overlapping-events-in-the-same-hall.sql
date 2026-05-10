3ffcac5da89a24bfb04d91145c22e725f3e7f3cb
Minh (Mark) Pham <99457952+MarkPhamm@users.noreply.github.com>
2026-05-10 15:51:34 -0400

    Time: 470 ms (26.03%), Space: 0B (100%) - LeetHub

--- 2494-merge-overlapping-events-in-the-same-hall/2494-merge-overlapping-events-in-the-same-hall.sql
+++ 2494-merge-overlapping-events-in-the-same-hall/2494-merge-overlapping-events-in-the-same-hall.sql
@@ -3,60 +3,30 @@
 -- gaps and island again lol
 -- similar to merge interval 
 
-with calc_prev_end_day as (
-select  
-    *,
-    lag(end_day) over(partition by hall_id order by start_day) prev_end_day
-    from HallEvents
-    order by end_day 
-), 
-calc_prev_end_day_range as ( 
-# the idea is that, if a larger end date is present, we will use that end date to check with the next start_date eg 
-# compare with prev_end_day_range instead of end_day
--- | hall_id | start_day  | end_day    | prev_end_day | prev_end_day_range | streak_start | group_id |
--- | ------- | ---------- | ---------- | ------------ | ------------------ | ------------ | -------- |
--- | 2       | 2022-12-08 | 2023-01-04 | null         | null               | 1            | 1        |
--- | 2       | 2022-12-09 | 2023-01-05 | 2023-01-04   | 2023-01-04         | 0            | 1        |
--- | 2       | 2022-12-13 | 2023-01-26 | 2023-01-05   | 2023-01-05         | 0            | 1        |
--- | 2       | 2023-01-04 | 2023-01-15 | 2023-01-26   | 2023-01-26         | 0            | 1        |
--- | 2       | 2023-01-20 | 2023-01-22 | 2023-01-15   | 2023-01-26         | 0            | 1        |
-    select  
-        *, 
-        max(prev_end_day) over(partition by hall_id order by start_day) prev_end_day_range
-    from calc_prev_end_day
-), 
-calc_streaK_start as (
+with calc_end_day_range as (
     select 
         *, 
-        case
-            when prev_end_day_range is null 
-                or prev_end_day_range < start_day
-            then 1
-            else 0 
-        end as streak_start 
-    from calc_prev_end_day_range
-),
+        # max of end date of all row before the current row 
+        max(end_day) over(partition by hall_id order by start_day rows between unbounded preceding and 1 preceding) as end_day_range 
+    from HallEvents
+), 
+calc_streak_start as (
+    select 
+        *, 
+        if(start_day > end_day_range,1,0) as streak_start
+    from calc_end_day_range
+), 
 
 calc_group as (
-    select
+    select 
         *, 
-        sum(streak_start) over(partition by hall_id order by start_day) group_id 
-    from calc_streaK_start
-
-),
-
-agg_group_id as (
-select 
-    hall_id, 
-    group_id, 
-    min(start_day) start_day, 
-    max(end_day) end_day
-from calc_group 
-group by 1,2 
+        sum(streak_start) over(partition by hall_id order by start_day) group_id
+    from calc_streak_start
 )
 
 select 
     hall_id, 
-    start_day, 
-    end_day
-from agg_group_id 
+    min(start_day) start_day, 
+    max(end_day) end_day
+from calc_group
+group by hall_id, group_id 
